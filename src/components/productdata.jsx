3917dafd925dd4d19f08965bc89fdc40c733bdfc
Royaltechsis <osenibunmi2023@gmail.com>
2024-12-05 20:27:17 +0100

    demo product data removed

--- src/components/productdata.jsx
+++ src/components/productdata.jsx
@@ -2,15 +2,15 @@
 
 const products = [
     { id: 1, name: "Smartphone", category: "Phones", price: "699", image: "https://via.placeholder.com/150?text=Smartphone" },
-    { id: 2, name: "Laptop", category: "Computers", price: "1299", image: "https://via.placeholder.com/150?text=Laptop" },
-    { id: 3, name: "Smartwatch", category: "Wearables", price: "199", image: "https://via.placeholder.com/150?text=Smartwatch" },
-    { id: 4, name: "Gaming Console", category: "Gaming", price: "499", image: "https://via.placeholder.com/150?text=Gaming+Console" },
-    { id: 5, name: "Headphones", category: "Audio", price: "$149", image: "https://via.placeholder.com/150?text=Headphones" },
-    { id: 6, name: "Bluetooth Speaker", category: "Audio", price: "99", image: "https://via.placeholder.com/150?text=Bluetooth+Speaker" },
-    { id: 7, name: "Drone", category: "Drones", price: "399", image: "https://via.placeholder.com/150?text=Drone" },
-    { id: 8, name: "Tablet", category: "Computers", price: "599", image: "https://via.placeholder.com/150?text=Tablet" },
-    { id: 9, name: "Camera", category: "Photography", price: "899", image: "https://via.placeholder.com/150?text=Camera" },
-    { id: 10, name: "Smart TV", category: "TVs", price: "999", image: "https://via.placeholder.com/150?text=Smart+TV" }
+    // { id: 2, name: "Laptop", category: "Computers", price: "1299", image: "https://via.placeholder.com/150?text=Laptop" },
+    // { id: 3, name: "Smartwatch", category: "Wearables", price: "199", image: "https://via.placeholder.com/150?text=Smartwatch" },
+    // { id: 4, name: "Gaming Console", category: "Gaming", price: "499", image: "https://via.placeholder.com/150?text=Gaming+Console" },
+    // { id: 5, name: "Headphones", category: "Audio", price: "$149", image: "https://via.placeholder.com/150?text=Headphones" },
+    // { id: 6, name: "Bluetooth Speaker", category: "Audio", price: "99", image: "https://via.placeholder.com/150?text=Bluetooth+Speaker" },
+    // { id: 7, name: "Drone", category: "Drones", price: "399", image: "https://via.placeholder.com/150?text=Drone" },
+    // { id: 8, name: "Tablet", category: "Computers", price: "599", image: "https://via.placeholder.com/150?text=Tablet" },
+    // { id: 9, name: "Camera", category: "Photography", price: "899", image: "https://via.placeholder.com/150?text=Camera" },
+    // { id: 10, name: "Smart TV", category: "TVs", price: "999", image: "https://via.placeholder.com/150?text=Smart+TV" }
   ];
   
   export default products;
